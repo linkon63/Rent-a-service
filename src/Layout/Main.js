3530996ddc8c69b05a9c766df4455836dc8191d5
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-01-24 19:25:28 +0600

    new branch added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -4,10 +4,10 @@ import Navbar from '../components/shared/Navbar/Navbar';
 
 const Main = () => {
     return (
-        <div>
+        <>
             <Navbar></Navbar>
             <Outlet></Outlet>
-        </div>
+        </>
     );
 };
 
