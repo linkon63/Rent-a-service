7d1e1d5bbd0984c1ba39d19279eb993f7bd51bc8
marziamostafa <marziamostafa@gmail.com>
2023-01-26 23:23:59 +0600

    updated

--- src/components/HomePage/Section3/Section3.jsx
+++ src/components/HomePage/Section3/Section3.jsx
@@ -7,9 +7,9 @@ import truck from '../../assets/icon/truck.png'
 const Section3 = () => {
     return (
         <div className=''>
-            <h1 className='text-center font-semibold text-2xl'>Car Service</h1>
+            <h1 className='text-center font-semibold text-2xl'>Our Service</h1>
             <div className='grid grid-cols-1 md:grid-cols-3 ml-10 '>
-                <div className="card w-96 bg-red-200 shadow-xl">
+                <div className="card w-96 bg-slate-400 shadow-xl">
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Car</h2>
                         <img src={car} alt="" className='w-56' />
@@ -18,7 +18,7 @@ const Section3 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card w-96 bg-green-200 shadow-xl">
+                <div className="card w-96 bg-slate-400 shadow-xl">
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Bus</h2>
                         <img src={bus} alt="" className='w-60' />
@@ -27,7 +27,7 @@ const Section3 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card w-96 bg-cyan-200 shadow-xl">
+                <div className="card w-96 bg-slate-400 shadow-xl">
                     <div className="card-body items-center text-center">
                         <h2 className="card-title ">Truck</h2>
                         <img src={truck} alt="" className='w-56' />
