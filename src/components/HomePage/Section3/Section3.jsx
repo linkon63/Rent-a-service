4133fda2e0b7c4dd10b7bc14a60994138ccb2f9a
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-02-23 09:00:52 +0600

    our service section change

--- src/components/HomePage/Section3/Section3.jsx
+++ src/components/HomePage/Section3/Section3.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import './Section3.css'
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-
 const Section3 = () => {
     return (
         <div className='mt-3'>
@@ -35,7 +31,7 @@ const Section3 = () => {
                     </div>
                     <div className="product-image">
 
-                        <img src="https://images.unsplash.com/photo-1606830733744-0ad778449672?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNocmlzdG1hcyUyMHRyZWV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <img src="https://i.ibb.co/p0sjp3X/13841402-2010-i123-005-car-headlights-AD-composition.jpg" alt="" />
 
 
                         <div className="info">
@@ -51,9 +47,7 @@ const Section3 = () => {
                     </div>
                 </div>
                 <div id="container">
-
                     <div className="product-details">
-
                         <h1>Bus</h1>
                         <span className="hint-star star">
                             <i className="fa fa-star" aria-hidden="true"></i>
@@ -62,8 +56,7 @@ const Section3 = () => {
                             <i className="fa fa-star" aria-hidden="true"></i>
                             <i className="fa fa-star-o" aria-hidden="true"></i>
                         </span>
-
-                        <p className="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.</p>
+                        <p className="information">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, veritatis?</p>
 
 
 
@@ -81,7 +74,7 @@ const Section3 = () => {
 
                     <div className="product-image">
 
-                        <img src="https://images.unsplash.com/photo-1606830733744-0ad778449672?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNocmlzdG1hcyUyMHRyZWV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <img src="https://i.ibb.co/k0b3czV/full-shot-disabled-man-near-bus.jpg" alt="" />
 
 
                         <div className="info">
@@ -110,7 +103,7 @@ const Section3 = () => {
                             <i className="fa fa-star-o" aria-hidden="true"></i>
                         </span>
 
-                        <p className="information mb-6">" Let's spread the joy , here is Christmas , the most awaited day of the year.</p>
+                        <p className="information mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, eaque.</p>
 
 
 
@@ -126,7 +119,7 @@ const Section3 = () => {
 
                     <div className="product-image">
 
-                        <img src="https://images.unsplash.com/photo-1606830733744-0ad778449672?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNocmlzdG1hcyUyMHRyZWV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <img src="https://i.ibb.co/kcV4PKQ/truck-long-vehicle-ready-delivering-transport.jpg" alt="" />
 
 
                         <div className="info">
