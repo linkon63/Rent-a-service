25851a46c3f9f8f49d4c2b5521ae09b70251c576
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-02-23 21:04:43 +0600

    section added

--- src/components/lib/CarsServicePages/CarsServicePage.jsx
+++ src/components/lib/CarsServicePages/CarsServicePage.jsx
@@ -4,23 +4,27 @@ import Gauges from '../../shared/Gauges/Gauges'
 
 export default function CarsServicePage() {
     return (
-        <div>
-            <h1>
-                Section design
-            </h1>
 
-            {/* Design Section */}
+        <div className='h-100'>
 
-            <div className='text-center'>
-                <h1>LIMIT YOUR SPEED AND BOOK THE CAR</h1>
+            <div class="grid grid-cols-2 content-center gap-4">
+                <div className=''>
+                    <div className='text-center'>
+                        <h1>LIMIT YOUR SPEED AND BOOK THE CAR</h1>
+                        <div>
+                            <Gauges />
+                        </div>
+                    </div>
+                </div>
                 <div>
-                    <Gauges />
+                    <h1>
+                        Section design
+                    </h1>
+                    <button style={{ border: "4px solid yellow;", background: "red" }}>
+                        <Link to={`/bookingService`}>Book Now</Link>
+                    </button>
                 </div>
             </div>
-
-            <button style={{ border: "4px solid yellow;", background: "red" }}>
-                <Link to={`/booking_service`}>Book Now</Link>
-            </button>
         </div>
     )
 }
