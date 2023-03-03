489dc3c1c379e072a5744da3b9a6bba5a5c21873
marziamostafa <marziamostafa@gmail.com>
2023-03-03 23:27:05 +0600

    slider updated

--- src/components/HomePage/Home/Home.jsx
+++ src/components/HomePage/Home/Home.jsx
@@ -13,67 +13,101 @@ import Section3 from '../Section3/Section3';
 
 import './Home.css'
 
+import video from '../../assets/background/pexels-ojyrai-7875596.mp4'
+
 const Home = () => {
     return (
-        <div>
-            <div className="carousel w-full h-screen" style={{ position: 'relative', zIndex: 0 }}>
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src={car1} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src={car2} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src={car3} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
+        // <div>
+        //     <div className="carousel w-full h-screen" style={{ position: 'relative', zIndex: 0 }}>
+        //         <div id="slide1" className="carousel-item relative w-full">
+        //             <img src={car1} className="w-full" alt="" />
 
+        //             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        //                 <a href="#slide4" className="btn btn-circle">❮</a>
+        //                 <a href="#slide2" className="btn btn-circle">❯</a>
+        //             </div>
+        //         </div>
+        //         <div id="slide2" className="carousel-item relative w-full">
+        //             <img src={car2} className="w-full" alt="" />
+        //             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        //                 <a href="#slide1" className="btn btn-circle">❮</a>
+        //                 <a href="#slide3" className="btn btn-circle">❯</a>
+        //             </div>
+        //         </div>
+        //         <div id="slide3" className="carousel-item relative w-full">
+        //             <img src={car3} className="w-full" alt="" />
+        //             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        //                 <a href="#slide2" className="btn btn-circle">❮</a>
+        //                 <a href="#slide1" className="btn btn-circle">❯</a>
+        //             </div>
+        //         </div>
+
+        //     </div>
+
+
+
+
+
+        //     {/* <Section1></Section1> */}
+        //     {/* <Section3></Section3> */}
+        //     {/* <Section2></Section2> */}
+        //     {/* <MissionVission></MissionVission> */}
+        //     {/* <Review></Review> */}
+        //     <Footer></Footer>
+        // </div >
+
+
+        // <div>
+        //     <div className="hero min-h-screen" >
+        //         <div className="hero-overlay bg-opacity-60"></div>
+        //         <div className="hero-content text-center text-neutral-content">
+        //             <div className="max-w-md">
+        //                 <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+        //                 <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        //                 <button className="btn btn-primary">Get Started</button>
+        //             </div>
+        //         </div>
+
+
+        //     </div>
+
+
+        //     <video autoPlay loop muted>
+        //         <source src={video} type='video/ogg' />
+        //     </video>
+        //     <script>
+        //         window.onload = function(){
+        //             document.getElementById("autoplay")
+        //         }
+        //     </script>
+        //     <Footer></Footer>
+        // </div>
+
+        <div>
+            <div>
+                <video autoPlay loop muted className='lg:h-screen lg:w-screen'>
+                    <source src={video} type='video/ogg' />
+                </video>
+                <script>
+                    window.onload = function(){
+                        document.getElementById("autoplay")
+                    }
+                </script>
             </div>
 
-            {/* <div className='md:flex hidden w-2/3 justify-center items-center -mt-28  p-4 ml-32 ' style={{ position: 'absolute', zIndex: 2 }}>
-                <div className='relative bg-blue-100 w-1/2 '>
-                    <img src={car5} alt="" className='img-round' />
-                    <p className='absolute top-1/2 left-3/5 text-black text-3xl font-semibold ' >Chose Your Car Based On</p>
-                </div>
-                <div className='grid grid-cols-2 gap-2 bg-blue-200 p-4'>
-                    <p className='border-2 h-10 p-2 border-slate-600 bg-red-100 text-center'>Address</p>
-                    <p className='border-2 h-10 p-2 border-slate-600 bg-red-100 text-center'>Date</p>
-                    <p className='border-2 h-10 p-2 border-slate-600 bg-red-100 text-center'>Time</p>
-                    <p className='border-2 h-10 p-2 border-slate-600 bg-red-100 text-center'>Model</p>
-                </div>
-            </div> */}
+            <div className="hero-content text-center text-neutral-content absolute top-28 lg:top-1/2 lg:left-1/3 flex flex-col items-center">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
 
-            {/* kon update Code 25/1/23 */}
-            {/* <section>
-                <h1 style={{ border: "2px solid black;" }}>Cars service</h1>
-
-                <div>
-                    <button style={{ border: "4px solid yellow;", background: "red" }}>
-                        <Link to={`/cars_services`}>Cars</Link>
-                    </button>
-                    <h1>Bus</h1>
-                    <h1>Track</h1>
                 </div>
-
-            </section> */}
-            {/* <Section1></Section1> */}
-            {/* <Section3></Section3> */}
-            {/* <Section2></Section2> */}
-            {/* <MissionVission></MissionVission> */}
-            {/* <Review></Review> */}
+            </div>
+            <div className='absolute top-20 lg:top-24 lg:left-24'>
+                <img className='w-20 lg:w-40' src="https://www.pngmart.com/files/22/Car-Logo-Transparent-PNG.png" alt="" />
+                <p className='font-semibold text-sm lg:text-2xl text-slate-400 font-serif'>Rent A Car</p>
+            </div>
             <Footer></Footer>
-        </div >
+        </div>
     );
 };
 
