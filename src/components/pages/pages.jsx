a50389b85dcb61478da85358f8294f577b458752
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-04-11 20:32:24 +0600

    remove unnessesary files

--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -1,9 +1,6 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
-
-import Main from "../Layout/Main"
+import Main from "../Layout/Main";
 import Login from "../auth/Login/Login";
 import Registration from "../auth/Registration/Registration";
 import Home from "../HomePage/Home/Home";
@@ -17,135 +14,133 @@ import DashboardNav from "../shared/Navbar/DashboardNav";
 import BookedPage from "../admin/Booked/BookedPage";
 import SettingPage from "../admin/Setting/SettingPage";
 import Contact from "../lib/ContactPage/Contact";
+import Dashboard from "../admin/Dashboard/Dashboard";
 
 const pages = [
-    {
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
         path: "/",
-        element: <Main />,
-        children: [
-            {
-                path: '/',
-                element: <Home />
-            }
-        ]
-    },
-    {
-        path: "/home",
-        element: <Main />,
-        children: [
-            {
-                path: '/home',
-                element: <Home />
-            }
-        ]
-    },
-    {
-        path: "/contact",
-        element: <Main />,
-        children: [
-            {
-                path: '/contact',
-                element: <Contact />
-            }
-        ]
-    },
-    {
-        path: "/services/:id",
-        element: <Main />,
-        children: [{
-            path: "/services/:id",
-            element:
-                <PrivateRoute>
-                    <ServicesPage />
-                </PrivateRoute>
-        }
-        ]
-    },
-    {
-        path: "/bookingService",
-        element: <Main />,
-        children: [{
-            path: "/bookingService/:name/:id",
-            element:
-                <PrivateRoute>
-                    <BookingService />
-                </PrivateRoute>
-        }
-        ]
-    },
-    {
-        path: "/booking",
-        element: <div>Hello booking!</div>,
-    },
-    {
-        path: "/payment",
-        element:
-            <PrivateRoute>
-                <PaymentForm />,
-            </PrivateRoute>
-    },
-    {
-        path: "/successful",
-        element:
-            <PrivateRoute>
-                <SuccessfulPage />,
-            </PrivateRoute>
-    },
-    {
-        path: "/registration",
-        element: <Registration />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/admin",
-        element:
-            <PrivateRoute>
-                <DashboardNav>
-                    <p>This is dashboard</p>
-                </DashboardNav>
-            </PrivateRoute>
-    },
-    {
-        path: "/dashboard",
-        element: <PrivateRoute>
-            {/* <DashboardNav> */}
-            <p>This is dashboard</p>
-            {/* </DashboardNav> */}
-        </PrivateRoute>,
-    },
-    {
-        path: "/admin/booked",
-        element:
-            <PrivateRoute>
-                <DashboardNav>
-                    <BookedPage />
-                </DashboardNav>
-            </PrivateRoute>
-    },
-    {
-        path: "/admin/setting",
-        element:
-            <PrivateRoute>
-                <DashboardNav>
-                    <SettingPage />
-                </DashboardNav>
-            </PrivateRoute>
-    },
-    {
-        path: "/nav",
-        element:
-            <PrivateRoute>
-                <DashboardNav></DashboardNav>
-            </PrivateRoute>
-    },
-    {
-        path: "*",
         element: <Home />,
-    },
-
-]
+      },
+    ],
+  },
+  {
+    path: "/home",
+    element: <Main />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/contact",
+    element: <Main />,
+    children: [
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/services/:id",
+    element: <Main />,
+    children: [
+      {
+        path: "/services/:id",
+        element: (
+          <PrivateRoute>
+            <ServicesPage />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/bookingService",
+    element: <Main />,
+    children: [
+      {
+        path: "/bookingService/:name/:id",
+        element: (
+          <PrivateRoute>
+            <BookingService />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  // TODO: It will work in future
+  //   {
+  //     path: "/payment",
+  //     element: (
+  //       <PrivateRoute>
+  //         <PaymentForm />,
+  //       </PrivateRoute>
+  //     ),
+  //   },
+  //   {
+  //     path: "/successful",
+  //     element: (
+  //       <PrivateRoute>
+  //         <SuccessfulPage />,
+  //       </PrivateRoute>
+  //     ),
+  //   },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/admin",
+    element: (
+      <PrivateRoute>
+        <DashboardNav>
+          <Dashboard />
+        </DashboardNav>
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/admin/booked",
+    element: (
+      <PrivateRoute>
+        <DashboardNav>
+          <BookedPage />
+        </DashboardNav>
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/admin/setting",
+    element: (
+      <PrivateRoute>
+        <DashboardNav>
+          <SettingPage />
+        </DashboardNav>
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/nav",
+    element: (
+      <PrivateRoute>
+        <DashboardNav></DashboardNav>
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "*",
+    element: <Home />,
+  },
+];
 export const router = createBrowserRouter(pages);
-
